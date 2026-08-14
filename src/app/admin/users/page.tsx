8b556464a912eced3335fdf7dd5ab3372c2a7dcd
ShameemSheiky <shameem363636@gmail.com>
2026-08-15 01:23:20 +0530

    Env Key missing Bug Fixed

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -21,7 +21,7 @@ import {
 import { useToast } from "@/providers/toast-provider";
 
 const API_BASE =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
 
 interface User {
   id: string;
